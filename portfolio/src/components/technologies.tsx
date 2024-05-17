12d443fd5ce4090849b387c2ba8638b41403e9ec
Tristan Giardini <111300903+Tristan-Giardini@users.noreply.github.com>
2024-05-17 10:44:03 -0400

    feat: restructure work and projects, finish pre-qa

--- portfolio/src/components/technologies.tsx
+++ portfolio/src/components/technologies.tsx
@@ -3,7 +3,7 @@ import Carousel from "./carousel"
 export default function Technologies() {
     return (
         <div>
-            <p className="text-xl lg:text-2xl mb-10">Technologies 👇</p>
+                  <div className="flex flex-row text-xl lg:text-2xl mb-10 md:mb-16 lg:mb-20 gap-2"><p className="">Technologies</p><p className="pt-1">👇</p></div>
             <Carousel />
         </div>
     )
