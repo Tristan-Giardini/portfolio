bbfe66dfa395a896fd3ef6728420c478934a5892
Tristan Giardini <111300903+Tristan-Giardini@users.noreply.github.com>
2024-05-20 23:54:44 -0400

    feat: change copy, change finger ani

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
     <>
       <div className="flex items-center lg:justify-normal lg:items-start lg:flex-row lg:pt-32 lg:pb-36 pt-14 pb-20 pr-0 pl-0 lg:pl-0 lg:w-[800px] gap-8">
         <p className="lg:text-4xl md:text-3xl text-2xl">
-        A full-stack developer based in Montreal, dedicated to creating user-centric and accessible websites.
+        A full-stack developer based in Montreal, dedicated to creating user-centric, accessible websites.
         </p>
       </div>
-      <div className="flex flex-row text-xl lg:text-2xl mb-10 md:mb-16 lg:mb-20 gap-2"><p className="font-semibold">Work & Projects</p><p className="pt-1">👇</p></div>
+      <div className="flex flex-row text-xl lg:text-2xl mb-10 md:mb-16 lg:mb-20 gap-2"><p className="font-semibold">Work & Projects</p><p className="finger pt-1">👇</p></div>
       <div className="flex flex-col-reverse lg:flex-row w-full pl-0 pr-0 md:pl-10 md:pr-10 lg:pl-40 lg:pr-10  gap-12 md:gap-20 lg:gap-0">
       <div className="flex flex-col gap-12 md:gap-20 lg:gap-28 px-5 lg:px-20">
         <Work
@@ -40,7 +40,7 @@ export default function Home() {
           company="Project"
           subtitle="Full-stack Development"
           work="Design & Development"
-          tech="React, CSS, Node.js, Express, MongoDB, OnRender"
+          tech="React, Styled Components, Node.js, Express, MongoDB"
           className="self-end"
           speed={screenWidth > 1024 ? -3 : 0}
           margin={screenWidth > 1024 ? "mt-20" : ""}
@@ -49,12 +49,12 @@ export default function Home() {
         />
         <Work
           image="/beccs.png"
-          link="https://rebecca-website.vercel.app"
+          link="https://rebecca-storm.com"
           title="Rebecca Storm"
           company="Freelance"
           subtitle="Front-end Development"
           work="Design & Development"
-          tech="React, CSS, Vercel"
+          tech="React, CSS"
           className="self-end"
           speed={screenWidth > 1024 ? -9 : 0}
           margin={screenWidth > 1024 ? "mb-20" : ""}
@@ -70,7 +70,7 @@ export default function Home() {
               company="HRVST"
               subtitle="Front-end Development"
               work="PDP Custom Integration"
-              tech="React, CSS, Vite, Shopify Liquid"
+              tech="React, CSS, Shopify Liquid, Vite"
               className="self-end"
               speed={screenWidth > 1024 ? 1 : 0}
               margin={screenWidth > 1024 ? "mb-12" : ""}
@@ -84,7 +84,7 @@ export default function Home() {
             company="HRVST"
             subtitle="Front-end Development"
             work="Custom Checkout Extension"
-            tech="React, CSS, Vite, Shopify Liquid"
+            tech="React, CSS, Shopify Liquid, Vite"
             className="self-end"
             speed={screenWidth > 1024 ? 1 : 0}
             margin=""
