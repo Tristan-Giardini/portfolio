12e714293fc29155c4288b712e88f56d06b81b42
Tristan Giardini <111300903+Tristan-Giardini@users.noreply.github.com>
2024-09-10 13:22:01 -0400

    feat: add april-april and new sticker

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Work from "@/components/work";
 import Image from "next/image";
@@ -6,77 +6,99 @@ import Technologies from "@/components/technologies";
 import useSize from "@/hooks/useSize";
 
 export default function Home() {
-
-  const screenWidth: number = useSize()
+  const screenWidth: number = useSize();
 
   return (
     <>
       <div className="flex items-center lg:justify-normal lg:items-start lg:flex-row lg:pt-32 lg:pb-36 pt-14 pb-20 pr-0 pl-0 lg:pl-0 lg:w-[800px] gap-8">
         <p className="lg:text-4xl md:text-3xl text-2xl">
-        A full-stack developer based in Montreal, dedicated to creating user-centric, accessible websites.
+          A full-stack developer based in Montreal, dedicated to creating
+          user-centric, accessible websites.
         </p>
       </div>
-      <div className="flex flex-row text-xl lg:text-2xl mb-10 md:mb-16 lg:mb-20 gap-2"><p className="font-semibold">Work & Projects</p><p className="finger pt-1">👇</p></div>
-      <div className="flex flex-col-reverse lg:flex-row w-full pl-0 pr-0 md:pl-10 md:pr-10 lg:pl-40 lg:pr-10  gap-12 md:gap-20 lg:gap-0">
-      <div className="flex flex-col gap-12 md:gap-20 lg:gap-28 px-5 lg:px-20">
-        <Work
-          link="https://app.kbdinsurance.com/"
-          title="KBD Insurance Quoter"
-          company="HRVST"
-          subtitle="Full-stack Development"
-          work="Reskin & Refactor"
-          tech="TypeScript, CSS, Node.js, Express, MongoDB, Heroku"
-          image="/kbdinsurance.png"
-          className="self-end"
-          speed={screenWidth > 1024 ? -1 : 0}
-          margin="mt-20"
-          id="parallax"
-          screen={screenWidth}
-        />
-        <Work
-          image="/brmtl.png"
-          link="https://www.bathroomsmtl.ca/"
-          title="BathroomsMTL"
-          company="Project"
-          subtitle="Full-stack Development"
-          work="Design & Development"
-          tech="React, Styled Components, Node.js, Express, MongoDB"
-          className="self-end"
-          speed={screenWidth > 1024 ? -3 : 0}
-          margin={screenWidth > 1024 ? "mt-20" : ""}
-          id="parallax"
-          screen={screenWidth}
-        />
-        <Work
-          image="/beccs.png"
-          link="https://rebecca-storm.com"
-          title="Rebecca Storm"
-          company="Freelance"
-          subtitle="Front-end Development"
-          work="Design & Development"
-          tech="React, CSS"
-          className="self-end"
-          speed={screenWidth > 1024 ? -9 : 0}
-          margin={screenWidth > 1024 ? "mb-20" : ""}
-          id="parallax"
-          screen={screenWidth}
-        />
+      <div className="flex flex-row text-xl lg:text-2xl mb-10 md:mb-16 lg:mb-20 gap-2">
+        <p className="font-semibold">Work & Projects</p>
+        <p className="finger pt-1">👇</p>
+      </div>
+      <div className="flex flex-col-reverse lg:flex-row w-full pl-0 pr-0 md:pl-10 md:pr-10 lg:pl-40 lg:pr-10  gap-12 md:gap-20 lg:gap-0 md:mb-0 lg:mb-96">
+        <div className="flex flex-col gap-12 md:gap-20 lg:gap-28 px-5 lg:px-20">
+          <Work
+            link="https://app.kbdinsurance.com/"
+            title="KBD Insurance Quoter"
+            company="HRVST"
+            subtitle="Full-stack Development"
+            work="Reskin & Refactor"
+            tech="TypeScript, CSS, Node.js, Express, MongoDB, Heroku"
+            image="/kbdinsurance.png"
+            className="self-end"
+            speed={screenWidth > 1024 ? -1 : 0}
+            margin="mt-20"
+            id="parallax"
+            screen={screenWidth}
+            isNew={false}
+          />
+          <Work
+            image="/brmtl.png"
+            link="https://www.bathroomsmtl.ca/"
+            title="BathroomsMTL"
+            company="Project"
+            subtitle="Full-stack Development"
+            work="Design & Development"
+            tech="React, Styled Components, Node.js, Express, MongoDB"
+            className="self-end"
+            speed={screenWidth > 1024 ? -2 : 0}
+            margin={screenWidth > 1024 ? "mt-20" : ""}
+            id="parallax"
+            screen={screenWidth}
+            isNew={false}
+          />
+          <Work
+            image="/beccs.png"
+            link="https://rebecca-storm.com"
+            title="Rebecca Storm"
+            company="Freelance"
+            subtitle="Front-end Development"
+            work="Design & Development"
+            tech="React, CSS"
+            className="self-end"
+            speed={screenWidth > 1024 ? -4 : 0}
+            margin={screenWidth > 1024 ? "mb-20" : ""}
+            id="parallax"
+            screen={screenWidth}
+            isNew={false}
+          />
         </div>
         <div className="flex flex-col gap-12 md:gap-20 lg:gap-32 px-5 lg:px-10">
           <Work
-              image="/surf2.png"
-              link="https://surfcustom.com/products/surfboard"
-              title="Surf Custom"
-              company="HRVST"
-              subtitle="Front-end Development"
-              work="PDP Custom Integration"
-              tech="React, CSS, Shopify Liquid, Vite"
-              className="self-end"
-              speed={screenWidth > 1024 ? 1 : 0}
-              margin={screenWidth > 1024 ? "mb-12" : ""}
-              id="parallax"
-              screen={screenWidth}
-            />
+            image="/aprilapril.png"
+            link="https://aprilapril.gallery"
+            title="april april"
+            company="Freelance"
+            subtitle="Font-end Development"
+            work="Custom Website Build"
+            tech="Next.js, TypeScript, Tailwind, Sanity"
+            className="self-end"
+            speed={screenWidth > 1024 ? 1 : 0}
+            margin={screenWidth > 1024 ? "mb-12" : ""}
+            id="parallax"
+            screen={screenWidth}
+            isNew={true}
+          />
+          <Work
+            image="/surf2.png"
+            link="https://surfcustom.com/products/surfboard"
+            title="Surf Custom"
+            company="HRVST"
+            subtitle="Front-end Development"
+            work="PDP Custom Integration"
+            tech="React, CSS, Shopify Liquid, Vite"
+            className="self-end"
+            speed={screenWidth > 1024 ? 1 : 0}
+            margin={screenWidth > 1024 ? "mb-12" : ""}
+            id="parallax"
+            screen={screenWidth}
+            isNew={false}
+          />
           <Work
             image="/wf.png"
             link="https://westmountflorist.com/"
@@ -90,7 +112,8 @@ export default function Home() {
             margin=""
             id="parallax"
             screen={screenWidth}
-        />
+            isNew={false}
+          />
         </div>
       </div>
       <div className="technologies mb-20">
--- portfolio/src/components/work.tsx
+++ portfolio/src/components/work.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Parallax from "./Parallax";
+import New from "./new";
 
 interface FeaturedWorkProps {
   title: string;
@@ -15,6 +16,7 @@ interface FeaturedWorkProps {
   company: string;
   work: string;
   screen: number;
+  isNew: boolean;
 }
 
 export default function Work({
@@ -29,31 +31,60 @@ export default function Work({
   margin,
   company,
   work,
-  screen
+  screen,
+  isNew,
 }: FeaturedWorkProps) {
-
   return (
     <>
-    <Parallax speed={speed} className={className} id={id}>
-    <div className={`project-card w-full p-6 md:p-10 rounded-lg lg:${margin} bg-gray-50`}>
-      <Link target="_blank" href={link}><Image src={image} alt="project image" width={800} height={800} className="rounded-lg"></Image></Link>
-      <div className="mt-10 mb-4 flex flex-col gap-1">
-        <p><span className="font-semibold">{title}</span> - <span className="italic">{company}</span></p> 
-        {screen > 768 ? 
-        <p className="text-sm mt-2"><span>{subtitle}</span> - <span>{work}</span></p>
-        : 
-        <>
-        <p className="text-sm mt-2">{subtitle}</p>
-        <p className="text-sm">{work}</p>
-        </>
-        }
-        <p className="text-xs">{tech}</p>
-      </div>
-      <div className="flex font-semibold flex-row gap-2 see-div">
-        <Link target="_blank" href={link}>See more</Link><span className="finger-ani">👈</span>
-      </div>
-    </div>
-    </Parallax>
+      <Parallax speed={speed} className={className} id={id}>
+        <div
+          className={`relative project-card w-full p-6 md:p-10 rounded-lg lg:${margin} bg-gray-50`}
+        >
+          <div className="flex flex-row justify-between">
+            <div>
+              <Link target="_blank" href={link}>
+                <Image
+                  src={image}
+                  alt="project image"
+                  width={800}
+                  height={800}
+                  className="rounded-lg"
+                ></Image>
+              </Link>
+              <div className="mt-10 mb-4 flex flex-col gap-1">
+                <p>
+                  <span className="font-semibold">{title}</span> -{" "}
+                  <span className="italic">{company}</span>
+                </p>
+                {screen > 768 ? (
+                  <p className="text-sm mt-2">
+                    <span>{subtitle}</span> - <span>{work}</span>
+                  </p>
+                ) : (
+                  <>
+                    <p className="text-sm mt-2">{subtitle}</p>
+                    <p className="text-sm">{work}</p>
+                  </>
+                )}
+                <p className="text-xs">{tech}</p>
+              </div>
+              <div className="flex font-semibold flex-row gap-2 see-div">
+                <Link target="_blank" href={link}>
+                  See more
+                </Link>
+                <span className="finger-ani">👈</span>
+              </div>
+            </div>
+            <div className="tag-container absolute">
+              {isNew && (
+                <div className="new-tag">
+                  <New />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </Parallax>
     </>
   );
 }
