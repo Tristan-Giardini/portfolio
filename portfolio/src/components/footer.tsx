1381f45147df4b22407ecedadfb64df932cdc092
Tristan Giardini <111300903+Tristan-Giardini@users.noreply.github.com>
2025-10-24 09:36:50 -0400

    Update CV and Pic to homepage

--- portfolio/src/components/footer.tsx
+++ portfolio/src/components/footer.tsx
@@ -1,64 +1,102 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
-import dynamic from "next/dynamic"
+import dynamic from "next/dynamic";
 
 const MediaQuery = dynamic(() => import("react-responsive"), {
-    ssr: false
-})
-
+  ssr: false,
+});
 
 export default function Footer() {
-
   return (
     <>
-    <MediaQuery minWidth={600}>
-      <div className="text-l bottom-0 flex gap-3 align-center font-semibold flex-row justify-between w-full pt-8 lg:pt-20">
-      <div>Tristan Giardini</div>
-      <div className="relative">
-      <Link target="_blank" rel="noopener noreferrer" href="/Tristan Giardini Resume.pdf" className="link-hover-footer" passHref>
-          Resume/CV
-      </Link>
-      </div>
-      <div className="flex flex-row gap-4 w-16 relative">
-        <div className="social-hover">
-        <Link href="https://www.linkedin.com/in/tristangiardini/" target="_blank">
-        <Image src="/linked-in.svg" alt="linked-in link" width={30} height={30}></Image>
-        </Link>
-        </div>
-        <div className="social-hover">
-        <Link href="https://github.com/Tristan-Giardini" target="_blank">
-        <Image src="/github.svg" alt="github link" width={30} height={30}></Image>
-        </Link>
-        </div>
-      </div>
-    </div>
-    </MediaQuery>
-    <MediaQuery maxWidth={599}>
-    <div className="font-semibold text-l bottom-0 flex flex-col pt-8">
-      <Link target="_blank" rel="noopener noreferrer" href="/Tristan Giardini Resume.pdf" className="link-hover-footer" passHref>
+      <MediaQuery minWidth={600}>
+        <div className="text-l bottom-0 flex gap-3 align-center font-semibold flex-row justify-between w-full pt-8 lg:pt-20">
+          <div>Tristan Giardini</div>
+          <div className="relative">
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="/TristanGiardiniResume.pdf"
+              className="link-hover-footer"
+              passHref
+            >
               Resume/CV
-      </Link>
-      <div className="flex align-center flex-row justify-between w-full">
-        <div>Tristan Giardini</div>
-        <div className="relative">
-      </div>
-      <div className="flex flex-row gap-4 w-16 relative">
-        <div className="social-hover">
-          <Link href="https://www.linkedin.com/in/tristangiardini/" target="_blank">
-            <Image src="/linked-in.svg" alt="linked-in link" width={30} height={30}></Image>
-          </Link>
+            </Link>
+          </div>
+          <div className="flex flex-row gap-4 w-16 relative">
+            <div className="social-hover">
+              <Link
+                href="https://www.linkedin.com/in/tristangiardini/"
+                target="_blank"
+              >
+                <Image
+                  src="/linked-in.svg"
+                  alt="linked-in link"
+                  width={30}
+                  height={30}
+                ></Image>
+              </Link>
+            </div>
+            <div className="social-hover">
+              <Link href="https://github.com/Tristan-Giardini" target="_blank">
+                <Image
+                  src="/github.svg"
+                  alt="github link"
+                  width={30}
+                  height={30}
+                ></Image>
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="social-hover">
-          <Link href="https://github.com/Tristan-Giardini" target="_blank">
-            <Image src="/github.svg" alt="github link" width={30} height={30}></Image>
+      </MediaQuery>
+      <MediaQuery maxWidth={599}>
+        <div className="font-semibold text-l bottom-0 flex flex-col pt-8">
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="/Tristan Giardini Resume.pdf"
+            className="link-hover-footer"
+            passHref
+          >
+            Resume/CV
           </Link>
+          <div className="flex align-center flex-row justify-between w-full">
+            <div>Tristan Giardini</div>
+            <div className="relative"></div>
+            <div className="flex flex-row gap-4 w-16 relative">
+              <div className="social-hover">
+                <Link
+                  href="https://www.linkedin.com/in/tristangiardini/"
+                  target="_blank"
+                >
+                  <Image
+                    src="/linked-in.svg"
+                    alt="linked-in link"
+                    width={30}
+                    height={30}
+                  ></Image>
+                </Link>
+              </div>
+              <div className="social-hover">
+                <Link
+                  href="https://github.com/Tristan-Giardini"
+                  target="_blank"
+                >
+                  <Image
+                    src="/github.svg"
+                    alt="github link"
+                    width={30}
+                    height={30}
+                  ></Image>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
-    </div>
-    </MediaQuery>
+      </MediaQuery>
     </>
   );
 }
--- portfolio/src/components/header.tsx
+++ portfolio/src/components/header.tsx
@@ -1,23 +1,32 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
 
-
 export default function Header() {
-
   return (
     <>
       <div className="flex flex-row justify-between w-full">
         <div className="header flex flex-col font-semibold text-l">
           <div>Tristan Giardini</div>
           <div className="relative">
-            <Link href="mailto:tristangiardini@gmail.com" className="link-hover">Contact</Link>
+            <Link
+              href="mailto:tristangiardini@gmail.com"
+              className="link-hover"
+            >
+              Contact
+            </Link>
           </div>
-        </div> 
-        <div>
-          <Image className="rounded-full" src="/square.png" alt="portrait" width={100} height={100}></Image>
         </div>
+        <Link href="/">
+          <Image
+            className="rounded-full"
+            src="/square.png"
+            alt="portrait"
+            width={100}
+            height={100}
+          ></Image>
+        </Link>
       </div>
     </>
   );
